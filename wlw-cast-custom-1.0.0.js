f6a142d3b8ccd3b3083d35ee51676bf3bec21ab7
ginsya <syara.crimson@gmail.com>
2015-04-07 00:15:53 +0900

    add first version.

---
+++ wlw-cast-custom-1.0.0.js
@@ -0,0 +1,128 @@
+var d = document;
+
+if (d.getElementById('wlw_custom')==null) {
+
+var p1 = d.querySelectorAll('.block_playdata_01_text');
+// 使用率 ... usage rate
+var ur = parseFloat(p1[0].innerHTML);
+// 勝利数 ... win count
+var wc = parseInt(p1[1].innerHTML);
+// 総撃破数 ... crush count
+var crc = parseInt(p1[2].innerHTML);
+// 総撤退数 ... withdraw count
+var wdc = parseInt(p1[3].innerHTML);
+
+var p2 = d.querySelectorAll('.block_playdata_02_text');
+// キャスト別評価(平均) ... total page
+var tp = parseFloat(p2[0].innerHTML);
+// 勝利時(平均) ... win page
+var wp = parseFloat(p2[1].innerHTML);
+// 敗北時(平均) ... lose page
+var lp = parseFloat(p2[2].innerHTML);
+
+// 獲得ナイス(平均) ... total nice
+var tn = parseFloat(p2[3].innerHTML);
+// 勝利時(平均) ... win nice
+var wn = parseFloat(p2[4].innerHTML);
+// 敗北時(平均) ... lose nice
+var ln = parseFloat(p2[5].innerHTML);
+
+// 敗北数 ... lose count
+var lc = 0;
+if ((tp-lp)!=0) {
+	lc = parseInt(Math.round((wp-tp)*wc/(tp-lp)));
+} 
+// 勝率 ... win rate
+var wr = 0;
+if ((wc+lc)!=0) {
+	wr = Math.round(wc/(wc+lc)*100*10)/10;
+}
+// Kill Ratio ... kill ratio
+var kr = 0;
+if (wdc!=0) {
+	kr = Math.round(crc/wdc*100)/100;
+}
+
+// キャストID ... cast id
+// 文字数圧縮のため、パラメータはcastを前提とする
+var q = window.location.search.substring(1);
+var ci = q.split("=")[1];
+var pci = "p" + ci;
+
+// キャストデータ ... cast data
+// 前回のキャストデータ ... pre cast data
+// 日時情報、使用率、勝利数、敗北数、勝率、総撃破数、総撤退数、Kill Ratio、
+// キャスト別評価(平均)、勝利時(平均)、敗北時(平均)、
+// 獲得ナイス(平均)、勝利時(平均)、敗北時(平均)
+var now = new Date().getTime();
+var cd = [now, ur, wc, lc, wr, crc, wdc, kr, tp, wp, lp, tn, wn, ln];
+var pcd = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var ppcd = pcd.concat();
+
+var day = 1000*3600*24;
+// Cookieの有効期限(365日間)
+var ex = new Date();
+ex.setTime(now+day*365);
+
+// Cookieの読み込み
+// 前回のキャストデータを取得
+if (d.cookie) {
+	var c = d.cookie.split(";");
+	for (var i = 0; i < c.length; i++) {
+		var kv = c[i].trim().split("=");
+		if (kv[0] == ci) {
+			pcd = unescape(kv[1]).split(":");
+		}
+		if (kv[0] == pci) {
+			ppcd = unescape(kv[1]).split(":");
+		}
+	}
+}
+
+if (cd[1]!=pcd[1] || cd[2]!=pcd[2]) {
+	d.cookie = ci + "=" + escape(cd.join(":")) + "; expires=" + ex.toUTCString();
+}
+if ((cd[0]-pcd[0])>day) {
+	d.cookie = pci + "=" + escape(pcd.join(":")) + "; expires=" + ex.toUTCString();
+} else {
+	pcd = ppcd;
+}
+
+// HTMLの書き換え
+var fi = d.querySelector('.frame_inner');
+var nfi = fi.cloneNode(true);
+nfi.id = "wlw_custom";
+var p = nfi.querySelectorAll('.clearfix');
+function insert(i, t1, t2) {
+	var e = p[0].cloneNode(true);
+	var t = e.getElementsByTagName('div');
+	t[0].innerHTML = t1;
+	t[1].innerHTML = t2;
+	nfi.insertBefore(e, p[i]);
+}
+insert(2,"敗北数",lc+"<span class=\"font_small\">敗</span>");
+insert(2,"勝率",wr+"%");
+insert(4,"Kill Ratio",kr);
+function diff(i, t) {
+	var iad = Math.round((cd[i]-pcd[i])*100)/100;
+	var pm = "±";
+	if (iad>0) {
+		pm = "+";
+	}
+	if (iad<0) {
+		pm = "−";
+		iad = Math.abs(iad);
+	}
+	t.innerHTML = t.innerHTML + " <span style=\"color:#ff0000;\" class=\"font_small\">(" + pm + iad + ")</span>";
+}
+var np1 = nfi.querySelectorAll('.block_playdata_01_text');
+for (var i = 0; i < 7; i++) {
+	diff(i+1, np1[i]);
+}
+var np2 = nfi.querySelectorAll('.block_playdata_02_text');
+for (var i = 0; i < 6; i++) {
+	diff(i+8, np2[i]);
+}
+fi.parentNode.replaceChild(nfi, fi);
+
+}
